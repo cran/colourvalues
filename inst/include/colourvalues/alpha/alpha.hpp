81035ef38bdd4f5a9ca97b63b012e71df0f24d23
David Cooley <dcooley@symbolix.com.au>
2019-11-15 16:40:02 +0000

    version 0.3.1

--- inst/include/colourvalues/alpha/alpha.hpp
+++ inst/include/colourvalues/alpha/alpha.hpp
@@ -25,10 +25,21 @@ namespace alpha {
     return ALPHA_UNKNOWN;    // #nocov
   }
 
-  inline Rcpp::NumericVector validate_alpha( Rcpp::NumericVector& a, int& alpha_type, int& x_size ) {
+  inline Rcpp::NumericVector validate_alpha( Rcpp::NumericVector& a, int& alpha_type ) {
+
+    if( a.size() < 1 ) {
+      Rcpp::stop("colourvalues - invalid alpha vector");
+    }
+
+    if ( alpha_type == ALPHA_UNKNOWN ) {                // #nocov
+      Rcpp::stop("colourvalues - Unknown alpha definition");   // #nocov
+    }
 
     // Issue 47
     Rcpp::NumericVector alpha = Rcpp::clone( a );
+    if ( alpha_type == ALPHA_PALETTE ) {
+      return alpha;
+    }
 
     if ( alpha_type == ALPHA_CONSTANT ) {
       if ( alpha[0] >= 0 && alpha[0] < 1 ) {
@@ -38,10 +49,7 @@ namespace alpha {
       Rcpp::NumericVector alpha_full( 5, alpha[0] ); // initialise with 5 vals (so i can create a spline object);
       return alpha_full;
 
-    } else if ( alpha_type == ALPHA_PALETTE ) {
-      return alpha;
-
-    } else if (alpha_type == ALPHA_VECTOR ) {
+    } else if ( alpha_type == ALPHA_VECTOR ) {
 
       colourvalues::scale::rescale( alpha );
       alpha = alpha * 255;
@@ -50,24 +58,19 @@ namespace alpha {
       int n_alpha = alpha.size();
 
       if ( n_alpha < 5 ) {
-        int to_fill = 5 - n_alpha;
         double mean_alpha = Rcpp::mean( alpha );
-        Rcpp::NumericVector alpha_full( 5 );
+        Rcpp::NumericVector alpha_fill( 5, mean_alpha );
         int i = 0;
         for ( i = 0; i < n_alpha; i++ ) {
-          alpha_full[i] = alpha[i];
+          alpha_fill[i] = alpha[i];
         }
-        for ( i = 1; i <= to_fill; i++ ) {
-          alpha_full[ n_alpha + to_fill ] = mean_alpha;
-        }
-        return alpha_full;
+        return alpha_fill;
       }
       return alpha;
-
-    } else if ( alpha_type == ALPHA_UNKNOWN ) {                // #nocov
-      Rcpp::stop("colourvalues - Unknown alpha definition");   // #nocov
     }
-    return 0;  // #nocov never reached
+
+    Rcpp::NumericVector out;
+    return out;  // #nocov never reached
   }
 
 } // namespace alpha
